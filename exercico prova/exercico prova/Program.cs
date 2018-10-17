5e020e8edf010efb720a0a9589ba5a6c9280a0ca
LemuelB <36803456+LemuelB@users.noreply.github.com>
2018-10-16 21:27:40 -0300

    Add files via upload

---
+++ exercico prova/exercico prova/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_prova
+{
+    class Program
+    {
+        
+        static int Maior(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+
+        }
+        static int Maior2(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
+
+
+        static bool Par(int numero)
+        {
+            
+            int resto = numero % 2;
+            if (resto == 0)
+                return true;
+            else
+                return false;
+        }
+
+        static string Par2(int numero)
+        {
+            
+            int resto = numero % 2;
+            if (resto == 0)
+                return "par";
+            else
+                return "impar";
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maior(1, 2));
+            Console.WriteLine(Maior2(3, 4));
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
